46431b2075a7ed4763090a1d799204d7cd87a671
Mohammed Minhaj <nijulinux@gmail.com>
2024-05-18 17:23:06 +0530

    script.js added

---
+++ script.js
@@ -0,0 +1,6 @@
+function hidechat(){
+    var chat = document.querySelector(".chat-box");
+    chat.style.display = 'none';
+    var chat_svg = document.querySelector(".close-box-svg");
+    chat_svg.style.display = 'none';
+}
