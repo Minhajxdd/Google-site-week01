53fff15d52f6fb35cee41b974ec246768fd099a6
Mohammed Minhaj <nijulinux@gmail.com>
2024-05-18 18:30:23 +0530

    chat pop layout and js added

--- script.js
+++ script.js
@@ -4,3 +4,22 @@ function hidechat(){
     var chat_svg = document.querySelector(".close-box-svg");
     chat_svg.style.display = 'none';
 }
+function popUp(){
+    var chat = document.querySelector(".chat-box");
+    chat.style.display = 'none';
+    var chat_svg = document.querySelector(".close-box-svg");
+    chat_svg.style.display = 'none';
+    var go = document.querySelector(".chat");
+    go.style.display = 'none';
+    setTimeout(function() {
+        var pop = document.querySelector(".chat-pop");
+        pop.style.display = 'block'; 
+    }, 500);  
+}
+function popdown(){
+    var pop = document.querySelector(".chat-pop");
+    pop.style.display = 'none'; 
+    var go = document.querySelector(".chat");
+    go.style.display = 'block';
+    go.style.display = 'flex';
+}
