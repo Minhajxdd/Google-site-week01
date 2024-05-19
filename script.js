458708b47efe206db6ac86b9f356ed072d33f14b
Mohammed Minhaj <nijulinux@gmail.com>
2024-05-19 17:19:21 +0530

    600px svg's animations & effects added

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ function hidechat(){
     var chat_svg = document.querySelector(".close-box-svg");
     chat_svg.style.display = 'none';
 }
+
 function popUp(){
     var chat = document.querySelector(".chat-box");
     chat.style.display = 'none';
@@ -14,10 +15,35 @@ function popUp(){
     var pop = document.querySelector(".chat-pop");
     pop.style.display = 'block';   
 }
+
 function popdown(){
     var pop = document.querySelector(".chat-pop");
     pop.style.display = 'none'; 
     var go = document.querySelector(".chat");
     go.style.display = 'block';
     go.style.display = 'flex';
+}
+
+function navpop() {
+    var menu = document.querySelector('.menuBar');
+    var close = document.querySelector('.xBar');
+    menu.style.transform = 'rotate(180deg)';
+    
+    setTimeout(function() {
+        menu.style.display = 'none';
+        close.style.display = 'block';
+        menu.style.transform = 'rotate(0deg)';
+    }, 500);
+   
+}
+function navgo(){
+    var menu = document.querySelector('.menuBar');
+    var close = document.querySelector('.xBar');
+    close.style.transform = 'rotate(-180deg)';
+
+    setTimeout(function(){
+        close.style.display = 'none';
+        menu.style.display = 'block';
+        close.style.transform = 'rotate(0deg)';
+    },500);
 }
