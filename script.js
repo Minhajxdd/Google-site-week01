7f35d41398a2829b1b83d068683959d2175bc593
Mohammed Minhaj <nijulinux@gmail.com>
2024-05-22 12:21:33 +0530

    Small patches and 600 drop added

--- script.js
+++ script.js
@@ -27,11 +27,13 @@ function popdown(){
 function navpop() {
     var menu = document.querySelector('.menuBar');
     var close = document.querySelector('.xBar');
+    var drop = document.querySelector('.drop600');
     menu.style.transform = 'rotate(180deg)';
     
     setTimeout(function() {
         menu.style.display = 'none';
         close.style.display = 'block';
+        drop.style.display = 'block';
         menu.style.transform = 'rotate(0deg)';
     }, 500);
    
@@ -39,11 +41,26 @@ function navpop() {
 function navgo(){
     var menu = document.querySelector('.menuBar');
     var close = document.querySelector('.xBar');
+    var drop = document.querySelector('.drop600');
     close.style.transform = 'rotate(-180deg)';
 
     setTimeout(function(){
         close.style.display = 'none';
         menu.style.display = 'block';
+        drop.style.display = 'none';
         close.style.transform = 'rotate(0deg)';
     },500);
+}
+
+
+window.addEventListener('resize', checkWidth);
+window.addEventListener('load', checkWidth);
+
+function checkWidth(){
+    var off = document.querySelector('.drop600');
+
+    if(window.innerWidth >= 650){
+        off.style.display = 'none';
+        navgo();
+    }
 }
